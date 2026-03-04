58b0a5f94c9b0b87194f40e0a2f83074cb93279d
Vinicius Magalhães de Souza Sena <viniciussna@gmail.com>
2026-03-04 13:51:15 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -17,7 +17,18 @@ blue_palette = [
 ]
 
 LISTA_MOEDAS = ['USD', 'EUR', 'GBP', 'CHF', 'CAD']
-tickers = ['^BVSP', 'VALE3.SA', 'PETR4.SA', 'PRIO3.SA', 'WEGE3.SA']
+tickers = ['^BVSP', 'VALE3.SA', 'PETR4.SA', 'PRIO3.SA', 'WEGE3.SA',
+        "ABEV3","ALOS3","ASAI3","AZUL4","B3SA3","BBAS3","BBDC3","BBDC4","BBSE3",
+        "BPAC11","BRAP4","BRFS3","BRKM5","CASH3","CCRO3","CIEL3","CMIG4","CMIN3",
+        "COGN3","CPFE3","CPLE6","CRFB3","CSAN3","CSMG3","CSNA3","CVCB3","CYRE3",
+        "DXCO3","ECOR3","EGIE3","ELET3","ELET6","EMBR3","ENEV3","ENGI11","EQTL3",
+        "EZTC3","FLRY3","GGBR4","GOAU4","GOLL4","HAPV3","HYPE3","IGTI11","IRBR3",
+        "ITSA4","ITUB4","JBSS3","KLBN11","LREN3","LWSA3","MGLU3","MRFG3","MRVE3",
+        "MULT3","NTCO3","PCAR3","PETR3","PETR4","PETZ3","PRIO3","QUAL3","RADL3",
+        "RAIL3","RAIZ4","RDOR3","RENT3","RRRP3","SANB11","SBSP3","SLCE3","SMTO3",
+        "SOMA3","SUZB3","TAEE11","TIMS3","TOTS3","UGPA3","USIM5","VALE3","VBBR3",
+        "VIVT3","WEGE3","YDUQ3"
+    ]
 today = datetime.datetime.now()
 fixed_year = 2024
 jan_1 = datetime.date(fixed_year, 1, 1)
